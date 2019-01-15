65c7b1b16de812e58b2e78175aa125de90d0314b
Smolyn <DS93656@AD1.PROD>
2019-01-15 18:11:46 -0500

    File 1 from earlier package project.

---
+++ 01_write-installed-packages.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(dplyr)
+## deja vu from earlier!
+
+## create a data frame of your installed packages
+## hint: installed.packages() is the function you need
+packages <- as.data.frame(installed.packages())
+
+## optional: select just some of the variables, such as
+##   * Package
+##   * LibPath
+##   * Version
+##   * Priority
+##   * Built
+
+packages <- packages %>%
+  select(Package, LibPath, Version, Priority, Built)
+
+## write this data frame to data/installed-packages.csv
+## hint: readr::write_csv() or write.table()
